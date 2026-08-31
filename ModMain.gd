710e1e2629b7e375b2b31bbc6837167048146fcc
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-08-30 19:05:02 -0700

    Updated version

--- ModMain.gd
+++ ModMain.gd
@@ -10,8 +10,8 @@ var modPath:String = get_script().resource_path.get_base_dir() + "/"
 # Required var for the replaceScene() func to work
 var _savedObjects := []
 const MOD_VERSION_MAJOR = 1
-const MOD_VERSION_MINOR = 1
-const MOD_VERSION_BUGFIX = 3
+const MOD_VERSION_MINOR = 2
+const MOD_VERSION_BUGFIX = 0
 const MOD_VERSION_METADATA = "Hev Patch"
 
 # Initialize the mod
